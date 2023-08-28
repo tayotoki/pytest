3018a5acf62748407e3deebe37f4ca13eaa91714
Constantine Putilov <putilov.constantine@yandex.ru>
2023-08-28 21:46:40 +0500

    REFACTOR: make utils package 100% covered by tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,7 +5,10 @@ class TestArrs:
     def test_get(self):
         assert arrs.get([1, 2, 3], 1, "test") == 2
         assert arrs.get([], 0, "test") == "test"
+        assert arrs.get([1, 2], 100, "test") == "test"
 
     def test_slice(self):
         assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
         assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        assert arrs.my_slice([], ) == []
+        assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
